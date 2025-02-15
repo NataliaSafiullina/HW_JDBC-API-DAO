65a995da0462c630427573761a6ae2cef5870db1
NataliaSafiullina <nfsafiullina@gmail.com>
2025-02-15 20:42:56 +0700

    Delete DBProps

--- src/main/java/ru/safiullina/HW_JDBC_API_DAO/repository/DBProps.java
+++
@@ -1,7 +0,0 @@
-package ru.safiullina.HW_JDBC_API_DAO.repository;
-
-public class DBProps {
-    public static final String DB_URL = "jdbc:mysql://localhost:3306/netology";
-    public static final String DB_USER = "root";
-    public static final String DB_PASS = "pass";
-}
